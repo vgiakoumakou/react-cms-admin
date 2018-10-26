844006b26a1126a832db5094a5e0fc5c5df1869d
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2018-10-26 23:05:03 +0300

    Revert "NavBar, Pages, EditPage & NewPage Components. Routing is also added."
    This reverts commit 3e86ff7c449df4e6bd80de9ed19aed09c35d7d26.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./components/navbar";
-import Pages from "./components/pages";
-import NewPage from "./components/newpage";
-import EditPage from "./components/editpage";
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,16 +7,6 @@ class App extends Component {
     return (
       <React.Fragment>
         <main className="container">
-          <div>
-            <NavBar />
-            <div className="content">
-              <Switch>
-                <Route path="/pages" component={Pages} />
-                <Route path="/newpage" component={NewPage} />
-                <Route path="/editpage/:id" component={EditPage} />
-              </Switch>
-            </div>
-          </div>
         </main>
       </React.Fragment>
     );
