b1c4877a99bc822cb88bf91b476df6ae67e9f6cc
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2018-10-26 22:53:40 +0300

    Initial project incl. bootstrap and routing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="container">
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
